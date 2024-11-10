c25353f38059b30ee4187db3bbee54bf6d254be8
luka <lukagogokhia345@gmail.com>
2024-11-10 13:26:13 +0400

    second commit

---
+++ day 40/homework/python.py
@@ -0,0 +1,29 @@
+# შექმენით greed ფუნქცია რომელიც მომხმარებელს მიესლამება
+def greet():
+    name = input("what is your name? : ")
+    
+    print("hello " + name + "," + " how are you? ")
+greet()
+
+#გააკეთეთ sandwith_make ფუნქცია რომელების არგუმენტების დახმარებით გააკეთებთ თქვენს ბუტერბროტს
+def sandwich_make():
+    print("ingedients:")
+    print("2 pieces of bread")
+    print("sauce you want")
+    print("cheese")
+    print("chicken fillet")
+    print("tomato")
+    print("some letucce")
+    print("how to make:")
+    print("take one piece of bread")
+    print("put the sauce on it")
+    print("then put the chicken")
+    print("then cut the tomato and put one slice on it")
+    print("then put some lettuce")
+    print("and then put some cheese on it")
+    print("put the second piece of bread on it")
+    print("toast it all")
+    print("toast for 10 minutes")
+    print("put it on a plate,take the drink out of the refrigiarator and enjoy!")
+
+sandwich_make()
