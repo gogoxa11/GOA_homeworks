c582d6137f7f5485e15fe09bc9d27f31a975214f
luka <lukagogokhia345@gmail.com>
2025-07-03 17:13:31 +0400

    second commit

---
+++ day 68/homework/homework2.py
@@ -0,0 +1,3 @@
+def split_by_a(sentence):
+    return sentence.split("ა")
+print(split_by_a("პატარძეული"))
---
+++ day 68/homework/homework3.py
@@ -0,0 +1,4 @@
+def join_with_star(words):
+    return "*".join(words)
+
+print(join_with_star(["ვაშლი", "მსხალი", "ატამი"]))
