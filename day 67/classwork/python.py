d3113ae4ef39d172e53ca8dfa38d995423c4efa6
luka <lukagogokhia345@gmail.com>
2025-09-05 19:15:33 +0400

    second commit

---
+++ day 67/classwork/python.py
@@ -0,0 +1,13 @@
+sentence1 = "hello*world*i*am*goga"
+words1 = sentence1.split("*")
+print(words1)
+
+sentence2 = "this is my big string with several words"
+words2 = sentence2.split(" ")
+print(words2)
+
+sentence3 = "Python is very cool"
+chars = sentence3.split()
+new_sentence = " ".join(sentence3)
+print(sentence3)
+print(new_sentence)
