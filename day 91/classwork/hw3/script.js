2bca6f12a112e97d68d6ab3d53259f4149462279
luka <lukagogokhia345@gmail.com>
2025-11-27 21:20:23 +0400

    second commit

---
+++ day 91/classwork/hw3/script.js
@@ -0,0 +1,5 @@
+// შექმენით 5 ელემენტიანი სია. და მე-3 ინდექსზე დაამატეთ "Goa" და ბოლო ელემენტი წაშალეთ.
+let array = ["goal", "oriented", "programming", "academy", "GOA"]
+array.splice(3, 1, "Goa")
+array.splice(4, 1)
+alert(array)
